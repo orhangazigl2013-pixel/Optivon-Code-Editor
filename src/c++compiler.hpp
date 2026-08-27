184b621a138c43e7b03594e5fd6c934e887e2f6e
Orhan Gazi <orhangazigl2013@gmail.com>
2026-08-27 16:40:35 +0300

    Delete src directory

--- src/c++compiler.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CPP_COMPILER_HPP
-#define CPP_COMPILER_HPP
-
-#include <windows.h>
-
-// F5 tuşuna basıldığında çağrılacak ana fonksiyon
-void RunCode(HWND hwndOwner, const wchar_t* filePath);
-
-#endif
